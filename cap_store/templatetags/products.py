29a9d07e2d3242c2e4b6994c7e0e670d6fb9ec78
patrick aubin <paubin12@gmail.com>
2013-03-01 12:35:18 -0600

    added a new template tags

--- cap_store/templatetags/products.py
+++ cap_store/templatetags/products.py
@@ -2,10 +2,13 @@ from django import template
 
 register = template.Library()
 
-from cap_store.models import Category 
+from cap_store.models import Category, Product
 
 @register.assignment_tag
 def get_all_categories():
     return Category.objects.all() 
 
 
+@register.assignment_tag
+def get_all_products():
+    return Product.objects.filter(publish=True)
