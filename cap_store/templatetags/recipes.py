09b3e3113e07be6bd654eebbd2ac7dcdf3988d6d
patrick aubin <patrick@capacitr.com>
2013-03-05 17:20:22 -0600

    added category filter to templatetags

--- cap_store/templatetags/recipes.py
+++ cap_store/templatetags/recipes.py
@@ -5,8 +5,8 @@ register = template.Library()
 from cap_store.models import Recipe
 
 @register.assignment_tag
-def get_all_recipes():
+def get_all_recipes(category=None):
+    if category:
+        return Recipe.objects.filter(publish=True, category__name=category)
     return Recipe.objects.filter(publish=True) 
-
-
 
