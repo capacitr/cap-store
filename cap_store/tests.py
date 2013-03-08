2e7a3d383be23e9ede07049bddba74ca27e83fab
patrick aubin <paubin12@gmail.com>
2013-03-08 15:24:56 -0600

    added blank templates

--- cap_store/tests.py
+++ cap_store/tests.py
@@ -4,7 +4,7 @@ when you run "manage.py test".
 
 Replace this with more appropriate tests for your application.
 """
-
+from django.test.client import Client
 from django.test import TestCase
 
 
@@ -17,3 +17,8 @@ class SimpleTest(TestCase):
 
 class AdminTest(TestCase):
     def test_import(self):
+        pass
+
+class ViewTest(TestCase):
+    def test_recipe_view(self):
+        client = Client()
