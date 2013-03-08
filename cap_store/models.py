67eac0638e367798931f4421e113aeb0173e8825
patrick aubin <paubin12@gmail.com>
2013-03-08 16:25:04 -0600

    updated models, added attributes dict for random attributes

--- cap_store/models.py
+++ cap_store/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 from thumbnail_works.fields import EnhancedImageField
-from django.contrib.auth.models import User
+from django.utils import simplejson
 
 from south.modelsinspector import add_introspection_rules
 add_introspection_rules([], ["^thumbnail_works\.fields\.EnhancedImageField"])
@@ -33,7 +33,6 @@ class Category(models.Model):
     
 
 class Product(models.Model):
-
     brand = models.CharField(max_length=255, blank=True)
 
     name = models.CharField(max_length=255)
@@ -46,8 +45,7 @@ class Product(models.Model):
             'avatar': dict(size='697x349'),
             'product1': dict(size='405x238'),
         }
-    )
-
+        )
 
     category_one = models.ForeignKey(Category, null=True, on_delete=models.SET_NULL, related_name='parent_category', blank=True)
     category_two = models.ForeignKey(Category, null=True, on_delete=models.SET_NULL, related_name='child_category', blank=True)
@@ -61,9 +59,12 @@ class Product(models.Model):
 
     rating = models.IntegerField(choices=SCORE_CHOICES, default=0)
 
-    ingredients = models.TextField()
-    nutritionals = models.TextField()
-    nutritional_image = models.ImageField(upload_to='uploads', blank=True)
+    attributes = models.TextField(blank=True)
+
+    @property
+    def attributes_dict(self):
+        return simplejson.loads(self.attributes)
+
     @property
     def rating_list(self):
         return range(1,6)
@@ -74,46 +75,6 @@ class Product(models.Model):
     def get_absolute_url(self):
         return ('get_product', (), {'product_slug' : self.slug} )
 
-class Attribute(models.Model):
-    pass
-
-class Coupon(models.Model):
-    image = EnhancedImageField(
-        upload_to = 'uploads',
-        process_source = dict(
-            size='897x449', sharpen=True, upscale=True, format='JPEG'),
-        thumbnails = {
-            'avatar': dict(size='697x349'),
-        }
-
-    )
-class Recipe(models.Model):
-
-    name = models.CharField(max_length=255)
-    description = models.TextField()
-
-    image = EnhancedImageField(
-        upload_to = 'uploads',
-        process_source = dict(
-            size='120x90', sharpen=True, upscale=True, format='JPEG'),
-        thumbnails = {
-            'avatar': dict(size='120x90'),
-        }
-    )
-
-    ingredients = models.TextField(blank=True)
-    directions = models.TextField(blank=True)
-
-    category = models.ForeignKey('cap_store.Category', null=True, on_delete=models.SET_NULL)
-
-    publish = models.BooleanField(default=False)
-
-    @models.permalink
-    def get_absolute_url(self):
-        return ('get_recipe', (), {
-            'recipe_id' : self.pk 
-        })
-
-    class Meta:
-        ordering = ['category__name']
-
+class Image(models.Model):
+    product = models.ForeignKey('Product')
+    image = models.ImageField(upload_to="images")
