95e45d6dd349b31daed42f28ce009d457b05e4bf
patrick aubin <paubin12@gmail.com>
2013-04-01 09:56:51 -0500

    updated urls

--- cap_store/urls.py
+++ cap_store/urls.py
@@ -7,5 +7,5 @@ urlpatterns = patterns('',
     #url(r'^t/(?P<tag_slug>[\w-]+)/$', views.get_product_by_tag, name='store_product_by_tag'),
     url(r'^(?P<product_slug>[\w-]+)/$', views.get_product, name='store_product'),
     url(r'^category/(?P<category_name>[\w-]+)/$', views.products_by_category, name='product_by_category'),
-    url(r'^recipe/(?P<recipe_id>\d+)/$', views.recipe, name='recipe'),
+    url(r'^recipe/(?P<recipe_id>\d+)/$', views.get_recipe, name='recipe'),
 )
